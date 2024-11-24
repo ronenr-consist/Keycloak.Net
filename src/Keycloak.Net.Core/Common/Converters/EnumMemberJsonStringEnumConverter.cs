f337883a1ec38a6b565855ab46fac17d15223158
ronenr-consist <ronenr@consist.co.il>
2024-11-24 11:18:02 +0200

    Change to .net 6

--- src/Keycloak.Net.Core/Common/Converters/EnumMemberJsonStringEnumConverter.cs
+++ src/Keycloak.Net.Core/Common/Converters/EnumMemberJsonStringEnumConverter.cs
@@ -1,35 +1,60 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
-namespace Keycloak.Net.Common.Converters;
-
-/// <summary>
-/// Provides an AOT-compatible extension for <see cref="JsonStringEnumConverter"/> that adds support for <see cref="EnumMemberAttribute"/>.
-/// </summary>
-/// <typeparam name="TEnum">The type of the <see cref="TEnum"/>.</typeparam>
-public sealed class EnumMemberJsonStringEnumConverter<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields)] TEnum>()
-	: JsonStringEnumConverter<TEnum>(namingPolicy: ResolveNamingPolicy())
-	where TEnum : struct, Enum
+namespace Keycloak.Net.Common.Converters
 {
-	private static EnumMemberNamingPolicy? ResolveNamingPolicy()
-	{
-		var map = typeof(TEnum).GetFields(BindingFlags.Public | BindingFlags.Static)
-							   .Select(f => (f.Name, AttributeName: f.GetCustomAttribute<EnumMemberAttribute>()?.Value))
-							   .Where(pair => pair.AttributeName != null)
-							   .ToDictionary();
+    /// <summary>
+    /// Provides an extension for <see cref="JsonStringEnumConverter"/> that adds support for <see cref="EnumMemberAttribute"/>.
+    /// </summary>
+    /// <typeparam name="TEnum">The type of the <see cref="TEnum"/>.</typeparam>
+    public sealed class EnumMemberJsonStringEnumConverter<TEnum> : JsonConverter<TEnum>
+        where TEnum : struct, Enum
+    {
+        private static readonly Dictionary<string, string> _enumMemberMap;
+        private static readonly Dictionary<string, TEnum> _valueMap;
 
-		return map.Count > 0
-				   ? new EnumMemberNamingPolicy(map!)
-				   : null;
-	}
+        static EnumMemberJsonStringEnumConverter()
+        {
+            _enumMemberMap = typeof(TEnum).GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Select(f => (f.Name, AttributeName: f.GetCustomAttribute<EnumMemberAttribute>()?.Value))
+                .Where(pair => pair.AttributeName != null)
+                .ToDictionary(pair => pair.Name, pair => pair.AttributeName!);
 
-	private sealed class EnumMemberNamingPolicy(Dictionary<string, string> map) : JsonNamingPolicy
-	{
-		public override string ConvertName(string name)
-			=> map.GetValueOrDefault(name, name);
-	}
+            _valueMap = _enumMemberMap.ToDictionary(pair => pair.Value, pair => Enum.Parse<TEnum>(pair.Key));
+        }
+
+        public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.String)
+            {
+                throw new JsonException("Expected string token.");
+            }
+
+            string enumString = reader.GetString()!;
+            if (_valueMap.TryGetValue(enumString, out var value))
+            {
+                return value;
+            }
+
+            throw new JsonException($"Unable to parse \"{enumString}\" to Enum \"{typeof(TEnum).Name}\".");
+        }
+
+        public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
+        {
+            string name = value.ToString();
+            if (_enumMemberMap.TryGetValue(name, out var enumMemberName))
+            {
+                writer.WriteStringValue(enumMemberName);
+            }
+            else
+            {
+                writer.WriteStringValue(name);
+            }
+        }
+    }
 }
